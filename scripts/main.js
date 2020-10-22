bfffb011aaa96a8483d28d14ed2b05fb6e50019a
Alex Glover <alex@circulation.com>
2020-10-22 16:49:27 -0400

    no confetti on first

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   handleColorsFromURL()
 })
 
+let confettiCount = 0
 let lastFulfilled = false
 
 // Registering the Vue component for the color pickers.
@@ -605,25 +606,29 @@ function randomize(collection) {
 }
 
 function confetti() {
-  var $confettiItems = $('<div class="confetti"></div>'),    
-  colors = [
-      foregroundColor,
-      '#E9128C'
-  ],
-  height = 6.6,
-  width = 6.6;
+  if (confettiCount > 0) {
+    var $confettiItems = $('<div class="confetti"></div>'),    
+    colors = [
+        foregroundColor,
+        '#E9128C'
+    ],
+    height = 6.6,
+    width = 6.6;
+    
+    var scale, $confettiItem;
   
-  var scale, $confettiItem;
-
-  for(var i=0; i<100; i++) {
-    scale = Math.random() * 1.75 + 1;
-    $confettiItem = $('<svg class=\'confetti-item\' width=\'' + width * scale + '\' height=\'' + height * scale + '\' viewbox=\'0 0 ' + width + ' ' + height + '\'>\n  <use transform=\'rotate(' + Math.random() * 360 + ', ' + width / 2 + ', ' + height / 2 + ')\' xlink:href=\'#svg-confetti\' />\n</svg>');
-    $confettiItem.css({
-      'animation': Math.random() + .5 + 's ' + Math.random() * .5 + 's confettiFall ease-in both',
-      'color': randomize(colors),
-      'left': Math.random() * 100 + 'vw'
-    });
-    $confettiItems.append($confettiItem);
+    for(var i=0; i<100; i++) {
+      scale = Math.random() * 1.75 + 1;
+      $confettiItem = $('<svg class=\'confetti-item\' width=\'' + width * scale + '\' height=\'' + height * scale + '\' viewbox=\'0 0 ' + width + ' ' + height + '\'>\n  <use transform=\'rotate(' + Math.random() * 360 + ', ' + width / 2 + ', ' + height / 2 + ')\' xlink:href=\'#svg-confetti\' />\n</svg>');
+      $confettiItem.css({
+        'animation': Math.random() + .5 + 's ' + Math.random() * .5 + 's confettiFall ease-in both',
+        'color': randomize(colors),
+        'left': Math.random() * 100 + 'vw'
+      });
+      $confettiItems.append($confettiItem);
+    }
+    $('body').append($confettiItems)
   }
-  $('body').append($confettiItems)
+
+  confettiCount++
 }
